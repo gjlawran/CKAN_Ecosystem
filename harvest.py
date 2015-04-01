54475b4dd9897677df5441752a09c876990a66c6
Greg Lawrance <greg.lawrance@outlook.com>
2015-04-01 08:24:40 -0700

    fix to handle different API versions fix to handle trailing forward slashes on some URLs fix to extension dictionary creation code

--- harvest.py
+++ harvest.py
@@ -1,13 +1,16 @@
-__author__ = '"ageara'
+__author__ = 'gjlawran'
+# a script to harvest information on CKAN deployments
+
 import requests
 import json
 import time
 
-
+#output the results of the harvest to a text file
 surveyed = open('surveyed.out','w')
 surveyed.write("Current date & time " + time.strftime("%c")+"\n")
 surveyed.write("Sites not responding to request:\n")
 
+#use the OKF registry of CKAN instances as the list to harvest from
 r = requests.get('http://instances.ckan.org/config/instances.json')
 data = r.json()
 
@@ -16,11 +19,11 @@ extCollection = {}
 verCollection = {}
 OSCollection = {}
 
-# for each site determine status and save results to dicts
+# for each site determine status and store results to dicts
 
 for i in range(1,len(data)):
     #compose and make status request for the site
-    if data[i]['url'] : URLinst = data[i]['url'] +'/api/3/action/status_show'
+    if data[i]['url'] : URLinst = data[i]['url'].rstrip('//') +'/api/action/status_show'
     try:
         m = requests.get(URLinst)
 
@@ -34,10 +37,10 @@ for i in range(1,len(data)):
             verCollection.setdefault(m.json()['result']['ckan_version'],[]).append(data[i]['url'])
             OSCollection.setdefault(serverType,[]).append(data[i]['url'])
 
-            # create a dictionary of the extensions used with a list of sites as the values
-            noexten = len(m.json()['result']['extensions'])
-            for q in (0, noexten - 1):
-                extCollection.setdefault(m.json()['result']['extensions'][q],[]).append(data[i]['url'])
+            # create a dictionary of the extensions used
+            # key is extension, values are the URL of the site with extension
+            for eachext in m.json()['result']['extensions']:
+                extCollection.setdefault(eachext,[]).append(data[i]['url'])
 
         else:   # output those sites that failed
             print "FAIL#"+URLinst, m.status_code, serverType
